7b70ed8c7fd22eaa607ecd232b09d0948ab84fed
Macbook <macbook@MacBook-Air-2.local>
2017-04-07 08:20:17 +0530

    added view pager

--- app/src/main/java/com/example/macbook/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/example/macbook/criminalintent/CrimeListFragment.java
@@ -114,7 +114,7 @@ public class CrimeListFragment extends Fragment {
 
         @Override
         public void onClick(View v) {
-            Intent intent = CrimeActivity.newIntent(getActivity(), mCrime.getId());
+            Intent intent = CrimePagerActivity.newIntent(getActivity(), mCrime.getId());
             startActivity(intent);
             CrimeListFragment.this.mPosition = mPosition;
 
